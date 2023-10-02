9a72c05df0e788a4cc8e4b5fd1b7c50026c30743
Cauê Da Silva <105080324+caue191106@users.noreply.github.com>
2023-10-02 11:36:35 -0300

    responsividade

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,12 @@
+function menuShow()  {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open')
+    }
+}
+
 var button = document.getElementById('readbutton');
 
 button.addEventListener('click', function() {
